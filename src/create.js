913a7abc319a5b66d8d548eb9ddbd9570e42efde
hewenlin01 <hewenlin01@ky-tech.com.cn>
2019-12-07 11:24:20 +0800

    vue-cli

--- src/create.js
+++ src/create.js
@@ -1,26 +1,14 @@
 const path = require('path')
 const { promisify }  = require('util')
-const fs = require('fs')
-
+const shell = require('shelljs')
 const axios = require('axios')
 const ora = require('ora')
 const inquirer = require('inquirer')
 const downLoadGitRepo = require('download-git-repo')
-const Metalsmith = require('metalsmith')
 const chalk = require('chalk')
 
 let ncp = require('ncp')
 let { render } = require('consolidate').ejs
-
-const config = require('./config')
-const repoUrl = config('get', 'newRepo')
-
-console.log('配置', repoUrl)
-
-const cliName = 'nvue-cli'
-// const baseUrl_orgs = `https://api.github.com/orgs/${cliName}`// orgs
-// const baseUrl_repos = `https://api.github.com/repos/${cliName}` // tag
-
 
 // 转为promise格式
 const downLoadGit = promisify(downLoadGitRepo)
@@ -31,12 +19,6 @@ const downLoadDirectory = `${process.env[process.platform === 'darwin' ? 'HOME':
 
 const chalkSuccess = (text) => console.log(chalk.green(text))
 const chalkError = (text) => console.log(chalk.red(text))
-
-// 获取仓库列表
-// const fetchRepoList = async () => {
-//   const { data } = await axios.get(`${baseUrl_orgs}/repos`);
-//   return data
-// }
 
 // 获取版本信息列表
 const fetchTagsList = async (repo) => {
@@ -65,18 +47,9 @@ const wrapFetchAddLoading = (fn, message) => async (...args) => {
 }
 
 module.exports = async (projectName) => {
-  // let repos = await wrapFetchAddLoading(fetchRepoList, '正在获取模板中，请稍后...')()
-  // // 选择模板
-  // repos = repos.map(item => item.name)
-  // const { repo } = await inquirer.prompt({
-  //   name: 'repo',
-  //   type: 'list',
-  //   message: '请选择一个模板来创建项目',
-  //   choices: repos
-  // })
 
   // 选择版本
-  let tags = await wrapFetchAddLoading(fetchTagsList, '正在获取版本号，请稍等...')(repo)
+  let tags = await wrapFetchAddLoading(fetchTagsList, '正在获取版本号，请稍等...')('nvue')
   tags = tags.map(item => item.name)
 
   const { tag } = await inquirer.prompt({
@@ -85,56 +58,23 @@ module.exports = async (projectName) => {
     message: '请选择版本',
     choices: tags
   })
-
-
+  
   // 下载项目
   const target = await wrapFetchAddLoading(downLoad, 'download template')(tag)
-  console.log(target)
 
-  // 没有ask文件说明不需要编译
-  if(!fs.existsSync(target, 'ask.js')) {
-    // 将下载的文件拷贝到当前执行命令的目录下
-    await ncp(target, path.join(path.resolve(), projectName))
-    chalkSuccess(`创建项目成功 请执行\n cd ${projectName}`)
-  } else {
-    const askUrl = path.join(target, 'ask.js')
-    await new Promise((resolve, reject) => {
-      Metalsmith(__dirname)
-      .source(target)
-      .destination(path.resolve(projectName))
-      .use(async (files, metal, done) => {
-        const result = await inquirer.prompt(require(askUrl))
-        const data = metal.metadata()
-        // 将询问的结果放在metaData中保证在下一个中间价中可以获取到
-        Object.assign(data, result) 
-
-        delete files['ask.js']
-        done()
-      })
-      .use(async (files, metal, done) => {
-        Reflect.ownKeys(files).forEach(async (file) => {
-            let content = files[file].contents.toString() // 获取文件中的内容
-            if(file.includes('.js') || file.includes('.json')) {
-              if(content.includes('<%')) {
-                // 文件中用<% 我才需要编译
-                content = await render(content, metal.metadata()) // 用数据渲染模板
-                files[file].contents = Buffer.from(content)  // 渲染好的结果替换即可
-              }
-            }
-        })
-        // 不能少
-        done()
-      })
-      .build(err => {
-        // 执行中间件
-        if(!err) {
-          chalkSuccess(`创建项目成功 请执行cd ${projectName}`)
-          resolve()
-        } else {
-          chalkError(err)
-          reject()
-        }
-      })
-    })
-  }
+  // 将下载的文件拷贝到当前执行命令的目录下
+  let url = path.join(path.resolve(), projectName)
+  await ncp(target, url)
+  const { installWay } = await inquirer.prompt({
+    name: 'installWay',
+    type: 'list',
+    message: '请选择安装方式',
+    choices: [
+      { name: 'cnpm install' },
+      { name: 'yarn' },
+      { name: 'npm install' }
+    ]
+  })
+  shell.exec(`cd ${url} && ${installWay}`)
+  chalkSuccess(`创建项目成功 请执行\n cd ${projectName}`)
 }
